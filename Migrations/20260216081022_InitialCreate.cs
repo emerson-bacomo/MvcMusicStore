4a74ebbf4312161fd9530645320609320213bb3a
ACT-STUDENT <ACT-STUDENT@LAB2-PC1>
2026-02-16 16:36:38 +0800

    Add project files.

---
+++ Migrations/20260216081022_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMusic.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsBanner = table.Column<bool>(type: "bit", nullable: false),
+                    BannerDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    SoldAmount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMusic.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public required string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(30)]
+        public required string Category { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public required string Brand { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [Range(0, double.MaxValue, ErrorMessage = "Value cannot be negative.")]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Value cannot be negative.")]
+        public int Stock { get; set; }
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+        public string? Image { get; set; } = "https://placehold.co/600x400?text=No+Image";
+        public bool IsBanner { get; set; }
+        public string? BannerDescription { get; set; }
+
+        [Range(0, 5)]
+        public double Rating { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Value cannot be negative.")]
+        public int SoldAmount { get; set; }
+    }
+}
